e7aca851d2890cdf113b9998a41663d36593e9ed
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-08-17 16:01:57 -0500

    explore work

--- prepare.py
+++ prepare.py
@@ -7,14 +7,39 @@ import seaborn as sns
 from pydataset import data
 import statistics
 import acquire
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+import warnings
+warnings.filterwarnings("ignore")
 
-def prep_iris():
-    df = acquire.get_iris_data()
+
+
+def clean_iris(df):
     df = df.drop(columns = ['species_id'])
     df = df.rename(columns={"species_name": "species"})
     df_dummy = pd.get_dummies(df['species'], drop_first=True)
     df= pd.concat([df, df_dummy], axis = 1)
     return df
+
+def split_iris_data(df):
+    """
+    splits the data in train validate and test 
+    """
+    train, test = train_test_split(df, test_size = 0.2, random_state = 123, stratify = df.species)
+    train, validate = train_test_split(train, test_size=.3, random_state=123, stratify=train.species)
+    
+    return train, validate, test
+
+def prep_irs_data(df):
+    """
+    takes in a data from titanic database, cleans the data, splits the data
+    in train validate test and imputes the missing values for embark_town. 
+    Returns three dataframes train, validate and test.
+    """
+    df = clean_iris(df)
+    train, validate, test = split_iris_data(df)
+    #train, validate, test = impute_mode(train, validate, test) #nothing to impute
+    return train, validate, test
 
 def prep_telco():
     df = acquire.get_telco_data()
