52e00b97e5900b653b4aa8b6ca5fff83a02c574d
joshuamchavez2 <joshua.m.chavez2@gmail.com>
2021-08-20 00:13:43 -0500

    modeling

--- prepare.py
+++ prepare.py
@@ -41,10 +41,57 @@ def prep_irs_data(df):
     #train, validate, test = impute_mode(train, validate, test) #nothing to impute
     return train, validate, test
 
-def prep_telco():
+
+
+def clean_telco():
     df = acquire.get_telco_data()
     df = df.drop_duplicates()
     df = df.drop(columns = ['customer_id'])
     df_dummy = pd.get_dummies(df[['gender', 'partner', 'dependents', 'phone_service', 'multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies', 'paperless_billing'  ]], drop_first=[True, True, True, True, True, True, True, True, True, True, True, True])
     df= pd.concat([df, df_dummy], axis = 1)
     return df
+
+
+############## Titanic ##################
+
+def clean_titanic(df):
+    '''
+    This function will drop any duplicate observations, 
+    drop columns not needed, fill missing embarktown with 'Southampton'
+    and create dummy vars of sex and embark_town. 
+    '''
+    df.drop_duplicates(inplace=True)
+    df.drop(columns=['deck', 'embarked', 'class', 'age'], inplace=True)
+    df.embark_town.fillna(value='Southampton', inplace=True)
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], drop_first=False)
+    return pd.concat([df, dummy_df], axis=1)
+
+def split_titanic_data(df):
+    """
+    splits the data in train validate and test 
+    """
+    train, test = train_test_split(df, test_size = 0.2, random_state = 123, stratify = df.survived)
+    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train.survived)
+    
+    return train, validate, test
+
+def impute_mode(train, validate, test):
+    '''
+    impute mode for embark_town
+    '''
+    imputer = SimpleImputer(strategy='most_frequent')
+    train[['embark_town']] = imputer.fit_transform(train[['embark_town']])
+    validate[['embark_town']] = imputer.transform(validate[['embark_town']])
+    test[['embark_town']] = imputer.transform(test[['embark_town']])
+    return train, validate, test
+
+def prep_titanic_data(df):
+    """
+    takes in a data from titanic database, cleans the data, splits the data
+    in train validate test and imputes the missing values for embark_town. 
+    Returns three dataframes train, validate and test.
+    """
+    df = clean_titanic(df)
+    train, validate, test = split_titanic_data(df)
+    train, validate, test = impute_mode(train, validate, test)
+    return train, validate, test
